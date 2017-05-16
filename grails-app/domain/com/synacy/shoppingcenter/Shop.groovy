7be3522f6214b3b12225106c29cdce57306b05a0
len <len.laurito@synacy.com>
2017-05-16 08:56:20 +0800

    Initial commit - domain

---
+++ grails-app/domain/com/synacy/shoppingcenter/Shop.groovy
@@ -0,0 +1,20 @@
+package com.synacy.shoppingcenter
+
+class Shop {
+
+	Long id
+	String name
+	String description
+
+	static constraints = {
+		description nullable: true
+	}
+
+	static mapping = {
+		id generator: 'sequence', params: [sequence: 'shop_sequence']
+	}
+
+	static hasMany = [
+			tags: Tag
+	]
+}
---
+++ grails-app/domain/com/synacy/shoppingcenter/Tag.groovy
@@ -0,0 +1,20 @@
+package com.synacy.shoppingcenter
+
+class Tag {
+
+	Long id
+	String name
+
+	static constraints = {
+	}
+
+	static mapping = {
+		id generator: 'sequence', params: [sequence: 'tag_sequence']
+	}
+
+	static belongsTo = Shop
+
+	static hasMany = [
+			shops: Shop
+	]
+}
