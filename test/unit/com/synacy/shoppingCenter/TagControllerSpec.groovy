1101103915d3e4b7aed164c45bea2e0430b3608b
Steven Jhon Gil <steven.gil@synacy.com>
2017-05-17 14:37:45 +0800

    Finish basic test for the TagController

--- test/unit/com/synacy/shoppingCenter/TagControllerSpec.groovy
+++ test/unit/com/synacy/shoppingCenter/TagControllerSpec.groovy
@@ -9,6 +9,8 @@ import spock.lang.Specification
 @TestFor(TagController)
 class TagControllerSpec extends Specification {
 
+    TagService tagService = Mock()
+
     def setup() {
     }
 
@@ -16,5 +18,79 @@ class TagControllerSpec extends Specification {
     }
 
     void "test something"() {
+    }
+
+    void "fetchTag should respond with the Tag with the given id"(){
+        given:
+            Long tagId = 1L;
+            Tag tag = new Tag(name: "Jewelry")
+            tagService.fetchTagById(tagId) >> tag
+
+        when:
+            controller.fetchTag(tagId)
+
+        then:
+            reponse.status == HttpStatus.OK.value()
+            response.json.name == "Jewelry"
+    }
+
+    void "fetchAllTag should respond with all the Tags"(){
+        given:
+            Tag tag1 = new Tag(name: "Jewelry")
+            Tag tag2 = new Tag(name: "Clothes")
+            tagService.fetchAllTag() >> [tag1, tag2]
+
+        when:
+            controller.fetchAllTag()
+
+        then:
+            reponse.status == HttpStatus.OK.value()
+            response.json.size() == 2
+            reponse.json.find{it.name == "Jewelry"} != null
+            reponse.json.find{it.name == "Clothes"} != null
+    }
+
+    void "createTag should respond with the newly created Tag and its details"(){
+        String name = "Jewelry"
+        given:
+            request.json = [name: name]
+            Tag tag = new Tag(name: name)
+
+        when:
+            controller.createTag()
+
+        then:
+            response.status == HttpStatus.CREATED.value()
+            response.json.name == name
+    }
+
+    void "updateTag should respond with the update Tag and its details"(){
+        given:
+            Long tagId = 1L
+            String name = "Jewelry"
+            request.json = [name: name]
+            Tag tag = new Tag()
+            tagService.fetchTagById(tagId) >> tag
+
+
+        when:
+            controller.updateTag(tagId)
+
+        then:
+            reponse.status == HttpStatus.OK.value()
+            response.name == name
+    }
+
+    void "removeTag should remove the Tag with the given tagId"(){
+        given:
+            Long tagId = 1L
+            Tag tag = new Tag(name: "Jewelry")
+            tagService.fetchTagById(tagId) >> tag
+
+        when:
+            controller.removeTag(tagId)
+
+        then:
+            reponse.status == HttpStatus.NO_CONTENT.value()
     }
 }
