33585688d36a91e7abd90bab372d19c689cf952f
JR Comision <catalino.comision@outlook.com>
2017-05-18 21:55:47 +0800

    Add controller and service to Tag

---
+++ grails-app/controllers/com/synacy/shoppingcenter/tag/TagController.groovy
@@ -0,0 +1,76 @@
+package com.synacy.shoppingcenter.tag
+
+import com.synacy.shoppingcenter.InvalidRequestException
+import com.synacy.shoppingcenter.ResourceNotFoundException
+
+import org.springframework.http.HttpStatus
+
+class TagController {
+
+	static responseFormats = ['json']
+
+	TagService tagService
+
+	def fetchTag(Long tagId) {
+		Tag tag = tagService.fetchTagById(tagId)
+		respond(tag)
+	}
+	
+	def fetchAllTags() {
+		List<Tag> tags = tagService.fetchAllTags()
+		Integer tagCount = tagService.fetchTotalNumberOfTags()
+		Map<String, Object> tagDetails = [totalRecords: tagCount, tags: tags]
+		respond(tagDetails)
+	}
+
+	def createTag() {
+		String name = request.JSON.name ?: null
+
+		if (!name) {
+			throw new InvalidRequestException("Name should not be empty.")
+		}
+		
+		Integer tagCountByName = tagService.fetchTotalNumberOfTagsByName()
+		if (tagCountByName > 0) {
+			throw new InvalidRequestException("Existing tag with name '" + name + "' already existed.")
+		}
+		Tag tag = tagService.createNewTag(name)
+		
+		respond(tag, [status: HttpStatus.CREATED])
+	}
+
+	def updateTag(Long tagId) {
+		String name = request.JSON.name ?: null
+
+		if (!name) {
+			throw new InvalidRequestException("Invalid request.")
+		}
+		Tag tag = tagService.fetchTagById(tagId)
+		if (!tag) {
+			throw new ResourceNotFoundException("Tag with id " + tagId + " not found")
+		}
+//		if (Tag.countByName(name) > 0) {
+//			throw new InvalidRequestException("Beware of duplication.")
+//		}
+		tag = tagService.updateTag(tag, name)
+		respond(tag)
+	}
+
+	def removeTag(Long tagId) {
+		Tag tag = tagService.fetchTagById(tagId)
+		tagService.deleteTag(tag)
+
+		render(status: HttpStatus.NO_CONTENT)
+	}
+
+	def handleResourceNotFoundException(ResourceNotFoundException e) {
+		response.status = HttpStatus.NOT_FOUND.value()
+		respond([error: e.getMessage()])
+	}
+
+	def handleInvalidRequestException(InvalidRequestException e) {
+		response.status = HttpStatus.UNPROCESSABLE_ENTITY.value()
+		respond([error: e.getMessage()])
+	}
+	
+}
---
+++ grails-app/services/com/synacy/shoppingcenter/tag/TagService.groovy
@@ -0,0 +1,46 @@
+package com.synacy.shoppingcenter.tag
+
+import com.synacy.shoppingcenter.ResourceNotFoundException
+import com.synacy.shoppingcenter.tag.Tag
+
+import grails.transaction.Transactional
+
+@Transactional
+class TagService {
+
+	public Tag fetchTagById(Long id) {
+		Tag tag = Tag.findById(id)
+		if (!tag) {
+			throw new ResourceNotFoundException("Tag with id " + id + " not found")
+		}
+		return tag
+	}
+	
+	public List<Tag> fetchAllTags() {
+		return Tag.list([sort: "id", order: "asc"])
+	}
+
+	public Tag createNewTag(String name) {
+		Tag tag = new Tag()
+		tag.name = name
+		return tag.save()
+	}
+
+	public updateTag(Tag tag, String name) {
+		tag.name = name
+		return tag.save()
+	}
+
+	public deleteTag(Tag tag) {
+		tag.delete()
+	}
+	
+	public Integer fetchTotalNumberOfTags() {
+		return Tag.count()
+	}
+	
+	public Integer fetchTotalNumberOfTagsByName(String name) {
+		return Shop.countByName(name)
+	}
+
+}
