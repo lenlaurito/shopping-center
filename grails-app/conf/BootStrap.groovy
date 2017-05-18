6dc88246a2ccbb7dc2ac19cf426fa84bcc19523f
sbanjoe25 <sbanjoe25@gmail.com>
2017-05-18 21:41:19 +0800

    add and initialize ShopMarshaller and TagMarshaller in BootStrap
    Signed-off-by: sbanjoe25 <sbanjoe25@gmail.com>

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,16 @@
+import com.synacy.shoppingcenter.ShopMarshaller
+import com.synacy.shoppingcenter.TagMarshaller
+
 class BootStrap {
 
     def init = { servletContext ->
+        initializeMarshallers()
     }
     def destroy = {
+    }
+
+    private void initializeMarshallers() {
+        new ShopMarshaller().register()
+        new TagMarshaller().register()
     }
 }
