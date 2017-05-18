b5d2632d4d8b78a151bccba7de392cc84d1dbaf2
JR Comision <catalino.comision@outlook.com>
2017-05-18 21:53:07 +0800

    Add Location to Shop with corresponding enum class

---
+++ src/groovy/com/synacy/shoppingcenter/shop/Location.groovy
@@ -0,0 +1,11 @@
+package com.synacy.shoppingcenter.shop
+
+
+public enum Location {
+	FIRST_FLOOR, SECOND_FLOOR, THIRD_FLOOR, FOURTH_FLOOR
+
+	public static Location valueOfLocation(String locationString) {
+		values().find { it.name().equalsIgnoreCase(locationString) }
+	}
+	
+}
