613367b7ae94110740c61ddd8675cf7b51f22ed5
sbanjoe25 <sbanjoe25@gmail.com>
2017-05-19 19:12:48 +0800

    add codes in ExceptionHandlingTrait
    Signed-off-by: sbanjoe25 <sbanjoe25@gmail.com>

--- src/groovy/com/synacy/shoppingcenter/ExceptionHandlingTrait.groovy
+++ src/groovy/com/synacy/shoppingcenter/ExceptionHandlingTrait.groovy
@@ -1,3 +1,5 @@
+package com.synacy.shoppingcenter
+
 import grails.validation.ValidationException
 import com.synacy.shoppingcenter.exceptions.*
 import org.springframework.http.HttpStatus
@@ -7,8 +9,17 @@ import javax.naming.LimitExceededException
 trait ExceptionHandlingTrait {
 
     def handleValidationException(ValidationException e) {
-        response.status = HttpStatus.CONFLICT.value()
-        respond([error: "Tag name already exist"])
+        response.status = HttpStatus.BAD_REQUEST.value()
+
+        String message = ""
+
+        if(e.getMessage().contains("cannot be null")) {
+            message = "Name field cannot be null"
+        } else {
+            message = "Tag name already exist"
+        }
+
+        respond([error: message])
     }
 
     def handleResourceNotFoundException(ResourceNotFoundException e) {
@@ -17,7 +28,7 @@ trait ExceptionHandlingTrait {
     }
 
     def handleExistingResourceException(ExistingResourceException e) {
-        response.status = HttpStatus.CONFLICT.value()
+        response.status = HttpStatus.BAD_REQUEST.value()
         respond([error: e.getMessage()])
     }
 
