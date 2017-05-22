dd4ef2211259237f8195a99cb14e200716ac7504
JR Comision <catalino.comision@outlook.com>
2017-05-22 14:32:20 +0800

    Add exception handling for request conflicts

---
+++ src/groovy/com/synacy/shoppingcenter/exception/RequestConflictViolationException.groovy
@@ -0,0 +1,9 @@
+package com.synacy.shoppingcenter.exception
+
+class RequestConflictViolationException extends RuntimeException{
+
+	public RequestConflictViolationException(String message) {
+		super(message)
+	}
+
+}
