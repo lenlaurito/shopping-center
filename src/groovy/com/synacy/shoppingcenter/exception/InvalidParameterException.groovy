7ea044d7b9e0313d05430132aa12b3802031eb3a
Kenichi Go Uang <kenichi.uang@synacy.com>
2017-05-22 19:25:42 +0800

    Add invalid parameter exception class
    Signed-off-by: Kenichi Go Uang <kenichi.uang@synacy.com>

---
+++ src/groovy/com/synacy/shoppingcenter/exception/InvalidParameterException.groovy
@@ -0,0 +1,13 @@
+/**
+ * Created by kenichigouang on 5/16/17.
+ */
+
+package com.synacy.shoppingcenter.exception
+
+class InvalidParameterException extends RuntimeException{
+
+    public InvalidParameterException(String message) {
+        super(message)
+    }
+
+}
