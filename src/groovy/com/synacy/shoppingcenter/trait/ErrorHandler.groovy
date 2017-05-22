6fdf6bd8bfdc64f02921ca92a8033caf9faf8ce1
Kenichi Go Uang <kenichi.uang@synacy.com>
2017-05-22 19:26:27 +0800

    Add handler for invalid parameter exceptions
    Signed-off-by: Kenichi Go Uang <kenichi.uang@synacy.com>

--- src/groovy/com/synacy/shoppingcenter/trait/ErrorHandler.groovy
+++ src/groovy/com/synacy/shoppingcenter/trait/ErrorHandler.groovy
@@ -3,6 +3,7 @@ package com.synacy.shoppingcenter.trait
 import com.synacy.shoppingcenter.exception.NoContentException
 import com.synacy.shoppingcenter.exception.InvalidFieldException
 import com.synacy.shoppingcenter.exception.InvalidUriException
+import com.synacy.shoppingcenter.exception.InvalidParameterException
 import org.springframework.http.HttpStatus
 
 trait ErrorHandler {
@@ -18,6 +19,11 @@ trait ErrorHandler {
     }
 
     def handleInvalidUriException(InvalidUriException e) {
+        response.status = HttpStatus.BAD_REQUEST.value()
+        respond([error: e.getMessage()])
+    }
+
+    def handleInvalidParameterException(InvalidParameterException e) {
         response.status = HttpStatus.BAD_REQUEST.value()
         respond([error: e.getMessage()])
     }
