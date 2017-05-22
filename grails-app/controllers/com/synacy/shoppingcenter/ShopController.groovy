816d313575f6023d8dbf96d8114f5e81e5e5f291
Kenichi Go Uang <kenichi.uang@synacy.com>
2017-05-22 19:17:19 +0800

    Change get parameters to accept tag names
    Signed-off-by: Kenichi Go Uang <kenichi.uang@synacy.com>

--- grails-app/controllers/com/synacy/shoppingcenter/ShopController.groovy
+++ grails-app/controllers/com/synacy/shoppingcenter/ShopController.groovy
@@ -2,6 +2,8 @@ package com.synacy.shoppingcenter
 
 import com.synacy.shoppingcenter.exception.NoContentException
 import com.synacy.shoppingcenter.trait.ErrorHandler
+import grails.validation.ValidationException
+import grails.converters.JSON
 import org.springframework.http.HttpStatus
 
 class ShopController implements ErrorHandler{
@@ -15,9 +17,9 @@ class ShopController implements ErrorHandler{
 
           Integer max = params.max ? Integer.parseInt(params.max) : null
           Integer offset = params.offset ? Integer.parseInt(params.offset) : null
-          Integer tagId = params.tagId ? Long.valueOf(params.tagId) : null
+          String tagName = params.tag ? String.valueOf(params.tag) : null
 
-          List<Shop> shops = shopService.fetchAllShops(max, offset, tagId)
+          List<Shop> shops = shopService.fetchAllShops(max, offset, tagName)
 
           Integer shopCount = shopService.fetchTotalNumberOfShops()
           Map<String, Object> paginatedShopDetails = [totalRecords: shopCount, records: shops]
@@ -45,17 +47,22 @@ class ShopController implements ErrorHandler{
           String name = request.JSON.name ?: null
           Location location = request.JSON.location ?: null
           String description = request.JSON.description ?: null
-          def tagIds = request.JSON.tagIds ?: null
+          def tagNames = request.JSON.tags ?: null
 
           List<Tag> tags = []
 
-          if(tagIds) {
-               tags = tagService.fetchTagsFromIds(tagIds)
+          if(tagNames) {
+              tags = tagService.fetchTagsByTagName(tagNames)
           }
 
-          Shop shop = shopService.createShop(name, location, description, tags)
+          try{
+              Shop shop = shopService.createShop(name, location, description, tags)
+              respond(shop, [status: HttpStatus.CREATED])
+          }catch(ValidationException e) {
+              response.status = 400
+              render([error: e.errors] as JSON)
+          }
 
-          respond(shop, [status: HttpStatus.CREATED])
     }
 
     def updateShop(Long shopId) {
@@ -63,21 +70,26 @@ class ShopController implements ErrorHandler{
           String name = request.JSON.name ?: null
           Location location = request.JSON.location ?: null
           String description = request.JSON.description ?: null
-          def tagIds = request.JSON.tagIds ?: null
-
-          List<Tag> tags = []
-
-          if(tagIds) {
-               tags = tagService.fetchTagsFromIds(tagIds)
-          }
+          def tagNames = request.JSON.tags ?: null
 
           Shop shop = shopService.fetchShopById(shopId)
 
           if(!shop) {
               throw new NoContentException("This shop does not exist.")
-          }else {
+          }
+
+          List<Tag> tags = []
+
+          if(tagNames) {
+              tags = tagService.fetchTagsByTagName(tagNames)
+          }
+
+          try{
               shop = shopService.updateShop(shop, name, location, description, tags)
               respond(shop)
+          }catch(ValidationException e) {
+              response.status = 400
+              render([error: e.errors] as JSON)
           }
     }
 
