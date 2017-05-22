5646ac97730b087d253db9286f1e797b9edc6459
Steven Jhon Gil <steven.gil@synacy.com>
2017-05-22 15:22:52 +0800

    Fix delete in ShopService

--- grails-app/services/com/synacy/shoppingCenter/ShopService.groovy
+++ grails-app/services/com/synacy/shoppingCenter/ShopService.groovy
@@ -58,7 +58,7 @@ class ShopService {
 
     void deleteShop(Long shopId){
         Shop shopToBeDeleted = fetchShopById(shopId)
-        shopToBeDeleted.delete()
+        if(shopToBeDeleted){shopToBeDeleted.delete()}
     }
 
     List<Tag> shopTagValidator(List<Long> tagIds){
